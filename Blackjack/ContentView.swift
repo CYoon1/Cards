c4577218b76e1287744779a6ba08362d2fd21b22
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2021-10-26 23:21:40 -0400

    Created Deck of Cards

--- Blackjack/ContentView.swift
+++ Blackjack/ContentView.swift
@@ -8,12 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    let deck = Deck()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            ForEach(deck.cards, id: \.id) {
+                Text("\($0.number.rawValue) of \($0.suit.rawValue)")
+            }
+        }
     }
 }
+enum Suit: String, CaseIterable {
+    case Spades = "♠️"
+    case Diamonds = "♦️"
+    case Hearts = "♥️"
+    case Clubs = "♣️"
+}
 
+enum Number: String, CaseIterable {
+    case two, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace
+}
+
+struct Card: Identifiable {
+    var suit: Suit
+    var number: Number
+    var id = UUID().uuidString
+}
+
+struct Deck: Identifiable {
+    var id = UUID().uuidString
+    var cards = [Card]()
+    init() {
+        for suit in Suit.allCases {
+            for number in Number.allCases {
+                self.cards.append(Card(suit: suit, number: number))
+            }
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
