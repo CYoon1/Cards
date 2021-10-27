a30f0cb93744a069d23521aec5aef77355e8dfae
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2021-10-26 23:00:39 -0400

    Initial Commit

---
+++ Blackjack/BlackjackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackjackApp.swift
+//  Blackjack
+//
+//  Created by Christopher Yoon on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct BlackjackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
